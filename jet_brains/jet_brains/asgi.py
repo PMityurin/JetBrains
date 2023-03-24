8248e844e2040845c1d03fb78eb104c78d79f9c5
PMityurin <mityurin96@gmail.com>
2023-03-24 11:30:29 +0300

    renamed the directory

--- jet_brains/jet_brains/asgi.py
+++ jet_brains/jet_brains/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for JetBrains project.
+ASGI config for jet_brains project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JetBrains.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jet_brains.settings')
 
 application = get_asgi_application()
--- jet_brains/jet_brains/wsgi.py
+++ jet_brains/jet_brains/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for JetBrains project.
+WSGI config for jet_brains project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JetBrains.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jet_brains.settings')
 
 application = get_wsgi_application()
